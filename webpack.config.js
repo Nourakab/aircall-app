83e7a3aaad93c15dcd917732e4f347c64ab05aea
Noura Kabbara <nourakabbara@gmail.com>
2024-07-21 23:28:25 -0400

    Update Webpack output directory to 'build

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config({ path: path.join(__dirname, ".env") });
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
     publicPath: "/",
   },
